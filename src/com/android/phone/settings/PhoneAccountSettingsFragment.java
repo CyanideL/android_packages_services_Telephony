b4b67d405c4e2ce69a59de6b4bc2225dca31f53b
eyosen <abittin@gmail.com>
2015-01-09 00:07:40 +0200

    TeleService: switches for checkboxes
    PS2: Change commit message
    
    Change-Id: Ife223b4609044602a96f922494b59280855a89da
    Signed-off-by: eyosen <abittin@gmail.com>

--- src/com/android/phone/settings/PhoneAccountSettingsFragment.java
+++ src/com/android/phone/settings/PhoneAccountSettingsFragment.java
@@ -7,11 +7,11 @@ import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Icon;
 import android.net.sip.SipManager;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.telecom.PhoneAccount;
 import android.telecom.PhoneAccountHandle;
 import android.telecom.TelecomManager;
@@ -72,7 +72,7 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
     private AccountSelectionPreference mDefaultOutgoingAccount;
 
     private ListPreference mUseSipCalling;
-    private CheckBoxPreference mSipReceiveCallsPreference;
+    private SwitchPreference mSipReceiveCallsPreference;
     private SipSharedPreferences mSipSharedPreferences;
 
     @Override
@@ -172,7 +172,7 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
             mUseSipCalling.setValueIndex(optionsValueIndex);
             mUseSipCalling.setSummary(mUseSipCalling.getEntry());
 
-            mSipReceiveCallsPreference = (CheckBoxPreference)
+            mSipReceiveCallsPreference = (SwitchPreference)
                     getPreferenceScreen().findPreference(SIP_RECEIVE_CALLS_PREF_KEY);
             mSipReceiveCallsPreference.setEnabled(SipUtil.isPhoneIdle(getActivity()));
             mSipReceiveCallsPreference.setChecked(
